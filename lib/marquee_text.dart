9b05be4f5e08d80a900c591351b10391161e8b00
zh28 <zhaozhuang.cn@gmail.com>
2020-07-14 00:04:58 +0800

    get parent constrains

--- lib/marquee_text.dart
+++ lib/marquee_text.dart
@@ -1,7 +1,57 @@
 library marquee_text;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MarqueeText extends StatefulWidget {
+  final String text;
+  final TextStyle textStyle;
+
+  MarqueeText({
+    Key key,
+    @required this.text,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  _MarqueeTextState createState() => _MarqueeTextState();
+}
+
+class _MarqueeTextState extends State<MarqueeText>
+    with SingleTickerProviderStateMixin {
+  // Animation<double> _animation;
+  // AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    var renderParagraph = RenderParagraph(
+      TextSpan(
+        text: widget.text,
+        style: widget.textStyle,
+      ),
+      softWrap: false,
+      maxLines: 1,
+      textDirection: TextDirection.ltr,
+      overflow: TextOverflow.visible,
+    );
+    renderParagraph.layout(BoxConstraints.tightForFinite());
+    print(renderParagraph.textSize.width);
+    print(renderParagraph.constraints.maxWidth);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      child: Container(
+        width: double.infinity,
+        child: Text(
+          widget.text,
+          style: widget.textStyle,
+          overflow: TextOverflow.visible,
+          softWrap: false,
+        ),
+      ),
+    );
+  }
 }
--- test/marquee_text_test.dart
+++ test/marquee_text_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:marquee_text/marquee_text.dart';
-
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+   /*  final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError); */
   });
 }
